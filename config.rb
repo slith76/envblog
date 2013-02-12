4485e7c89cb6a51d1199c83d342e1e4eb67f8aec
Oliver Friedrich <oliver.friedrich@igd-r.fraunhofer.de>
2013-02-12 13:35:31 +0100

    First deploy to github.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/envblog/"
+http_images_path = "/envblog/images"
+http_fonts_path = "/envblog/fonts"
+css_dir = "public/envblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
